a9840479f724bff550a5fb90e326fbcd3f82e123
Smipos <smiposbis@gmail.com>
2023-03-06 15:17:48 +0300

    Перенес функции в конец

--- GenIdtConsoleApp/Program.cs
+++ GenIdtConsoleApp/Program.cs
@@ -9,43 +9,6 @@ namespace GenIdtConsoleApp
     
     internal class Program
     {
-        static string[] GetQuestions(int countQuestions)
-        {
-            string[] questions = new string[countQuestions];
-            questions[0] = "Сколько будет два плюс два умноженное на два?";
-            questions[1] = "Бревно нужно распилить на 10 частей. Сколько распилов нужно сделать?";
-            questions[2] = "На двух руках 10 пальцев. Сколько пальцев на 5 руках?";
-            questions[3] = "Укол делают каждые полчаса. Сколько нужно минут, чтобы сделать три укола?";
-            questions[4] = "Пять свечей горело, две потухли. Сколько свечей осталось?";
-            return questions;
-        }
-
-        static int[] GetRightAnswer(int countQuestions)
-        {
-            int[] answers = new int[countQuestions];
-            answers[0] = 6;
-            answers[1] = 9;
-            answers[2] = 25;
-            answers[3] = 60;
-            answers[4] = 2;
-            return answers;
-        }
-
-        static string GiveDiagnose(int countRightAnswers)
-        {
-            string[] diagnoses = new string[6];
-            diagnoses[0] = "кретин";
-            diagnoses[1] = "идиот";
-            diagnoses[2] = "дурак";
-            diagnoses[3] = "нормальный";
-            diagnoses[4] = "талант";
-            diagnoses[5] = "гений";
-            string yourDiagnose = $"ваш диагноз: {diagnoses[countRightAnswers]}";
-            return yourDiagnose;
-        }
-
-
-
         static void Main()
         {
 
@@ -103,5 +66,41 @@ namespace GenIdtConsoleApp
                     break;
             }
         }
+
+        static string[] GetQuestions(int countQuestions)
+        {
+            string[] questions = new string[countQuestions];
+            questions[0] = "Сколько будет два плюс два умноженное на два?";
+            questions[1] = "Бревно нужно распилить на 10 частей. Сколько распилов нужно сделать?";
+            questions[2] = "На двух руках 10 пальцев. Сколько пальцев на 5 руках?";
+            questions[3] = "Укол делают каждые полчаса. Сколько нужно минут, чтобы сделать три укола?";
+            questions[4] = "Пять свечей горело, две потухли. Сколько свечей осталось?";
+            return questions;
+        }
+
+        static int[] GetRightAnswer(int countQuestions)
+        {
+            int[] answers = new int[countQuestions];
+            answers[0] = 6;
+            answers[1] = 9;
+            answers[2] = 25;
+            answers[3] = 60;
+            answers[4] = 2;
+            return answers;
+        }
+
+        static string GiveDiagnose(int countRightAnswers)
+        {
+            string[] diagnoses = new string[6];
+            diagnoses[0] = "кретин";
+            diagnoses[1] = "идиот";
+            diagnoses[2] = "дурак";
+            diagnoses[3] = "нормальный";
+            diagnoses[4] = "талант";
+            diagnoses[5] = "гений";
+            string yourDiagnose = $"ваш диагноз: {diagnoses[countRightAnswers]}";
+            return yourDiagnose;
+        }
+
     }
 }
