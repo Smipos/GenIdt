6df5cd8c5ce5b99216a6b532bd0fbd25e12e01e0
Smipos <smiposbis@gmail.com>
2023-03-08 16:44:36 +0300

    Добавил замечания по поводу программы

--- GenIdtConsoleApp/Program.cs
+++ GenIdtConsoleApp/Program.cs
@@ -32,6 +32,13 @@ namespace GenIdtConsoleApp
                     Console.WriteLine("Вопрос №" + (questionNumber + 1));
                     int randomQuestionIndex = random.Next(0, countQuestions);
 
+
+                    // постарайся реализовать функцию,
+                    // в которую мы помещаем словарь(или другую структуру данных) с вопросами и ответами,
+                    // а на выходе получаем рандомный вопрос с ответом
+                    // на следующий цикл - она тоже должна работать
+                    // посмотри по теме https://www.youtube.com/watch?v=KyFWqbRfWIA&list=PLQOaTSbfxUtD6kMmAYc8Fooqya3pjLs1N&ab_channel=%23SimpleCode 
+                    // 38 + 43 (обязательно) + 39, 44, 45 уроки (можно и другие тоже посмотреть, опционально)
                     mixedQuestions[questionNumber] = questions[randomQuestionIndex];
                     mixedAnswers[questionNumber] = answers[randomQuestionIndex];
 
@@ -59,6 +66,8 @@ namespace GenIdtConsoleApp
                     break;  
             }      
         }
+        // А попробуй добавить вопрос (лучше 2), где есть такой-же ответ как и в уже имеющемся вопросе (например 3 шестёрки)
+        // правильно ли будет себя вести программа?
         static Dictionary<int, string> GetQuestionAndAnswerDict()
         {
             var questionAndAnswerDict = new Dictionary<int, string>()
@@ -73,16 +82,18 @@ namespace GenIdtConsoleApp
         }
         static List<string> GetQuestions(Dictionary<int, string> questionAndAnswerDict)
         {
-            var questions = new List<string>();
+            var questions = new List<string>(); // эту строку можно объединить с нижней 
             questions = questionAndAnswerDict.Values.ToList();
             return questions;
         }
         static List<int> GetRightAnswer(Dictionary<int, string> questionAndAnswerDict)
         {
-            var answers = new List<int>();
+            var answers = new List<int>(); // эту строку можно объединить с нижней 
             answers = questionAndAnswerDict.Keys.ToList();
             return answers;
         }
+
+        // как будет вести себя постановка диагноза, если будут добавленны ещё вопросы
         static string GetDiagnose(int countRightAnswers)  
         {
             var diagnoses = new string[6];
@@ -95,18 +106,22 @@ namespace GenIdtConsoleApp
             string yourDiagnose = $"ваш диагноз: {diagnoses[countRightAnswers]}";
             return yourDiagnose;
         }
+        // название функции не соответствует её назначению
         public static Tuple<List<string>, List<int>> MixQuestionsAndAnswers(List<string> questions, List<int> answers, int randomQuestionIndex)
         {
             var mixedQuestions = MixQuestions(questions, randomQuestionIndex);
             var mixedAnswers = MixAnswers(answers, randomQuestionIndex);
             return new Tuple<List<string>, List<int>>(mixedQuestions, mixedAnswers);
         }
+        // название функции не соответствует её назначению. Она же не замешивает, а удаляет ответ по индексу
+        // зачем вообще нужна эта функция? почему бы не сделать просто RemoveAt()?
         public static List<string> MixQuestions(List<string> questions, int randomQuestionIndex)
         {
             var mixedQuestions = questions.ToList();
             mixedQuestions.RemoveAt(randomQuestionIndex);
             return mixedQuestions;
         }
+        // тоже самое что и по поводу функции выше
         public static List<int> MixAnswers(List<int> answers, int randomQuestionIndex)
         {
             var mixedAnswers = answers.ToList();
